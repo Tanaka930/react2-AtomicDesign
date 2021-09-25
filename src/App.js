13841d8c4bfec05526ae0d6a1ba6eb9442bed6ca
Tanaka930 <55372083+Tanaka930@users.noreply.github.com>
2021-09-25 19:02:43 +0900

    recoil

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React from "react";
+import "./styles.css";
+import { RecoilRoot } from "recoil";
 import { UserProvider } from "./providers/UserProvider";
 import { Router } from "./router/Router";
-import "./styles.css";
 
 export default function App() {
   return (
-    <UserProvider>
-      <Router />
-    </UserProvider>
+    <RecoilRoot>
+      <UserProvider>
+        <Router />
+      </UserProvider>
+    </RecoilRoot>
   );
 }
--- src/compornents/pages/Users.jsx
+++ src/compornents/pages/Users.jsx
@@ -1,7 +1,9 @@
-import React, { useContext } from "react";
+import React from "react";
 import styled from "styled-components";
+import { useRecoilState } from "recoil";
 
-import { UserContext } from "../../providers/UserProvider";
+// import { UserContext } from "../../providers/UserProvider";
+import { userState } from "../../store/userState";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
@@ -21,7 +23,8 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
-  const { userInfo, setUserInfo } = useContext(UserContext);
+  // const { userInfo, setUserInfo } = useContext(UserContext);
+  const [userInfo, setUserInfo] = useRecoilState(userState);
 
   const onClickSwitch = () => setUserInfo({ isAdmin: !userInfo.isAdmin });
 
